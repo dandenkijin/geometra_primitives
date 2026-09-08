800d2841143ba958ce839bf30fff00a211183374
dandenkijin <dandenkijin@gmail.com>
2026-09-08 15:56:36 -0400

    clean: remove target artifacts from index; keep .gitignore

--- target/debug/deps/test_pga-91cb7d84f8ac0fb5.d
+++
@@ -1,5 +0,0 @@
-/home/denkijin/workspace/geometra_pg/target/debug/deps/test_pga-91cb7d84f8ac0fb5.d: tests/test_pga.rs
-
-/home/denkijin/workspace/geometra_pg/target/debug/deps/test_pga-91cb7d84f8ac0fb5: tests/test_pga.rs
-
-tests/test_pga.rs:
